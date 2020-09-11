fc3569c6dbf472118e9e64408f2604402599ef55
Christian <christian@randleff.dk>
2020-09-11 14:56:37 +0200

    lavet knappen

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -2,4 +2,37 @@ import processing.core.PApplet;
 
 public class main extends PApplet {
     public static void main(String[] args) { PApplet.main("main"); }
+
+    ToggleKnap btnSize;
+
+    @Override
+    public void settings() {
+        //frame.setResizable(true);
+        size(500,500);
+
+    }
+
+    @Override
+    public void setup() {
+        btnSize = new ToggleKnap(this,10 + (height/6),0,height/6,height/6,"FullScreen/window" );
+    }
+
+    @Override
+
+
+    public void draw() {
+        btnSize.tegnKnap();
+        if(btnSize.erKlikket()){
+            println("omg!!!!!!!!!");
+            //frame.setSize((int)random(100,500),(int)random(100,500));
+        }
+
+    }
+
+    @Override
+    public void mousePressed() {
+        btnSize.registrerKlik(mouseX,mouseY);
+
+
+    }
 }
