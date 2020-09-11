09dfeb0ad4bdf3f181abcb67bd4813c2bfc01723
mariuss tokkebro <43348192+mariusstokkebro@users.noreply.github.com>
2020-09-11 12:11:29 +0200

    me epic

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,3 +4,5 @@ public class main extends PApplet {
     public static void main(String[] args) { PApplet.main("main"); }
 }
 //////https://www.youtube.com/watch?v=TZMp6ggt_Ls
+
+//skuffer er sejere end casper
