84330e7559d3b693eae18980fc55ac0c7e824145
casp8108 <54103603+casp8108@users.noreply.github.com>
2020-09-11 12:15:23 +0200

    asdadwaw

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,9 @@
 import processing.core.PApplet;
-//christian er en boomer :) :)
+//christian er en boomer xDD
 public class main extends PApplet {
     public static void main(String[] args) { PApplet.main("main"); }
 }
-//////https://www.youtube.com/watch?v=TZMp6ggt_Ls
+//////https://www.youtube.com/watch?v=TZMp6gg
 
 //skuffer er sejere end casper
+//du en boomer marius
