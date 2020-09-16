c753c093c2c1c5a37fbc41fc0ad992945b8cb558
casp8108 <54103603+casp8108@users.noreply.github.com>
2020-09-16 10:17:39 +0200

    Boogie woogie bil :=)

--- src/main/java/CarCPU.java
+++ src/main/java/CarCPU.java
@@ -1,25 +1,38 @@
 import processing.core.PApplet;
+
 import processing.core.PImage;
+
 import processing.core.PVector;
 
+
+
 public class CarCPU extends Car{
+
     PApplet p;
+    float rotationMin, rotationMax, rotation;
 
     CarCPU(PApplet p, PVector posit, PVector speed, PImage car) {
         super(p,posit, speed, car);
         this.p =p;
+
+        rotationMin = p.random(-1f,0f);
+        rotationMax = p.random(0f,1f);
     }
+
+
+
 
 
     void drawCar() {
-
-        p.rect(0, 0, 32, 16);
-        p.image(car, 0,0);
-
+        p.image(car, posit.x,posit.y);
     }
-
 
     void Movement() {
-        p.println("heheh bevægelse");
+        speed.rotate(rotation);
+        posit.x += speed.x * sMulti;
+        posit.y += speed.y * sMulti;
+
+        rotation = p.random(rotationMin,rotationMax);
     }
+
 }
