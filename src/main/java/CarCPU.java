a66e3957620ea6bab009ed22a632c95b03c26771
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2020-09-16 10:50:21 +0200

    Update CarCPU.java

--- src/main/java/CarCPU.java
+++ src/main/java/CarCPU.java
@@ -10,6 +10,7 @@ public class CarCPU extends Car{
 
     PApplet p;
     float rotationMin, rotationMax, rotation;
+    boolean collison;
 
     CarCPU(PApplet p, PVector posit, PVector speed, PImage car) {
         super(p,posit, speed, car);
@@ -22,12 +23,22 @@ public class CarCPU extends Car{
     void drawCar() {
 
         p.pushMatrix();
-            p.imageMode(p.CENTER);
-            p.translate(posit.x, posit.y);
-            p.rotate(speed.heading());
+        p.imageMode(p.CENTER);
+        p.translate(posit.x, posit.y);
+        p.rotate(speed.heading());
         p.image(car, 0,0);
         p.popMatrix();
 
+    }
+
+    void collsion(float rad) {
+        PVector lengthCar = new PVector(posit.x - p.width / 2, posit.y - p.height / 2);
+        lengthCar.mag();
+        p.line(p.width / 2, p.height / 2, p.width / 2 + lengthCar.x, p.height / 2 + lengthCar.y);
+
+        if (rad / 2 < lengthCar.mag()) {
+            collison = true;
+        }
     }
 
     void Movement() {
