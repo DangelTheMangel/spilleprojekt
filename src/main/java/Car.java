b4b9a955ae1aa1f6f5cdb7ca9ea1c8d1424c06ba
Christian Randleff <54103424+DangelTheMangel@users.noreply.github.com>
2020-09-16 10:49:15 +0200

    Update Car.java

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,51 +1,24 @@
 import processing.core.PApplet;
-
 import processing.core.PImage;
 import processing.core.PVector;
 
-
-
-public class Car {
-
+public class CarPlayer  extends Car {
     PApplet p;
 
-    //movement
 
-    boolean rotating = false;
+    CarPlayer(PApplet p,PVector posit, PVector speed, PImage car) {
 
-    PVector posit = new PVector(100, 200); //Position
-
-    PVector speed = new PVector(0, 1); //Speed
-
-    float sMulti; //Speed Multiplier (Det her skal bruges med speed powerups og sådan...
-
-    PImage car;
-
-    //meta
-
-    //  int points;
-
-
-
-    Car(PApplet p,PVector posit, PVector speed, PImage car) {
+        super(p,posit, speed, car);
         this.p =p;
-        this.posit = posit;
-
-        this.speed = speed;
-        this.car = car;
-        sMulti = 1;
     }
+    boolean collison = false;
 
 
-
+    @Override
     void Movement() {
 
-        p.println("Movement", posit, speed.x, "sMulti=" + sMulti);
-
         posit.x += speed.x * sMulti;
-
         posit.y += speed.y * sMulti;
-
 
         //Lav noget med at den skal samle powerups. Det her bliver nok rodet.
         //Jeg ville nok lave en række if-statements Yanderedev agtigt.
@@ -54,47 +27,68 @@ public class Car {
 
         //Lav noget med at den skal kunne falde af platformen.
 
-
-
     }
 
-
-
+    @Override
     void drawCar() {
-
-        p.fill(0,0,250);
+        p.fill(0, 0, 250);
         p.pushMatrix();
-
-        if(rotating) {
+        if (rotating == true) {
             p.rectMode(p.CENTER);
+            p.imageMode(p.CENTER);
             p.translate(posit.x, posit.y);
             p.rotate(speed.heading());
         }
-        p.rect(0, 0, 50, 20);
+
+        //p.rect(0, 0, 32, 16);
+        p.image(car, 0,0);
         p.popMatrix();
+
+        p.rectMode(p.CORNER);
     }
 
-
-//boomer
-    void Controls() { //Noget går galt her når man trykker på begge knapper på samme tid,
-                      // og vi skal have noget der tjekker om det sker og gør noget ved det.
+    void move() {
         if (p.keyPressed) {
-                boolean D = false;
-                boolean A = false;
-                if(p.keyPressed & p.key == 'd'){
-                    D=true;
-                }
-                if(p.keyPressed & p.key == 'a'){
-                    A=true;
-                }
-                if(A){
-                    speed.rotate(-0.1f);
-                    rotating = true;
-                }
-                else if(D){
-                speed.rotate(0.1f);}
-                else{
-                speed.rotate(0);}
+            if (p.key == 'a' || p.key == 'A') {
+                speed.x -= 2;
+            }
+            if (p.key == 'd' || p.key == 'D') {
+                speed.x += 2;
             }
         }
+
     }
+
+    void Controls() {
+        boolean D = false;
+        boolean A = false;
+
+        if (p.keyPressed && p.key == 'd' || p.keyPressed && p.key == 'D') {
+            D = true;
+        }
+
+        if (p.keyPressed && p.key == 'a' || p.keyPressed && p.key == 'A') {
+            A = true;
+        }
+
+        if (A) {
+            speed.rotate(-0.1f);
+            rotating = true;
+        } else if (D) {
+            speed.rotate(0.1f);
+        } else {
+            speed.rotate(0);
+        }
+    }
+
+
+    void collsion(float rad) {
+        PVector lengthCar = new PVector(posit.x - p.width / 2, posit.y - p.height / 2);
+        lengthCar.mag();
+        p.line(p.width / 2, p.height / 2, p.width / 2 + lengthCar.x, p.height / 2 + lengthCar.y);
+
+        if (rad / 2 < lengthCar.mag()) {
+            collison = true;
+        }
+    }
+}
