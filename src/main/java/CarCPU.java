a06df14f5488e290f35bc48c67f0c298ae865ec3
Christian <christian@randleff.dk>
2020-09-16 09:10:24 +0200

    Kristendom er forvildt

--- src/main/java/CarCPU.java
+++ src/main/java/CarCPU.java
@@ -1,2 +1,19 @@
-public class CarCPU {
+import processing.core.PImage;
+import processing.core.PVector;
+
+public class CarCPU extends Car{
+
+    CarCPU(PVector posit, PVector speed, PImage car) {
+        super(posit, speed, car);
+    }
+
+
+    void drawCar() {
+
+    }
+
+
+    void Movement() {
+
+    }
 }
